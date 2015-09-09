c909a5c78f2028f592e72b95d20fb11a0c6d62e9
riptorxxx <riptor@bk.ru>
2015-09-09 13:11:49 +0300

    Initial commit

---
+++ src/Raz1.java
@@ -0,0 +1,39 @@
+/**
+ * Created by ivanov_d on 03.08.2015.
+ */
+public class Raz1 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
